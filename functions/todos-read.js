5e3b39a23541151aa8e146f5595a5c3d141b9c11
Osama Ameer <osamaameer15@gmail.com>
2021-11-19 15:13:55 +0500

    crud functions

---
+++ functions/todos-read.js
@@ -0,0 +1,27 @@
+/* Import faunaDB sdk */
+const faunadb = require('faunadb')
+const getId = require('./utils/getId')
+const q = faunadb.query
+
+exports.handler = (event, context) => {
+  /* configure faunaDB Client with our secret */
+  const client = new faunadb.Client({
+    secret: "fnAETAr8UQACTFWS6jfgOzIdgYNHmHf7_lc_wVzE"
+  }) 
+  const id = getId(event.path)
+  console.log(`Function 'todo-read' invoked. Read id: ${id}`)
+  return client.query(q.Get(q.Ref(`classes/todos/${id}`)))
+    .then((response) => {
+      console.log('success', response)
+      return {
+        statusCode: 200,
+        body: JSON.stringify(response)
+      }
+    }).catch((error) => {
+      console.log('error', error)
+      return {
+        statusCode: 400,
+        body: JSON.stringify(error)
+      }
+    })
+}
